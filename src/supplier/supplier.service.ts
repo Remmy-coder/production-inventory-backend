aa85617fbb581efb77d018867277508f6d48167b
Remmy <remmyo@flexipgroup.com>
2023-11-02 12:40:53 +0100

    handle exceptions on the services

--- src/supplier/supplier.service.ts
+++ src/supplier/supplier.service.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   ConflictException,
   Inject,
   Injectable,
@@ -33,54 +34,63 @@ export class SupplierService extends AbstractService<Supplier> {
   async supplierRegistration(
     createSupplierDto: CreateSupplierDto,
   ): Promise<Supplier> {
-    const company = await this.companyService.findById(
-      createSupplierDto.companyId,
-    );
+    try {
+      const company = await this.companyService.findById(
+        createSupplierDto.companyId,
+      );
 
-    if (!company) {
-      throw new ConflictException('Company does not exists');
+      if (!company) {
+        throw new ConflictException('Company does not exists');
+      }
+
+      const contact = new SupplierContact();
+      //Object.assign(contact, createSupplierDto.supplierContact);
+      // await contact.save();
+
+      //const supplier = new Supplier();
+      // Object.assign(supplier, createSupplierDto);
+      // supplier.supplierContact = contact;
+      // supplier.company = company;
+
+      return this.create(createSupplierDto, Supplier, (dto) => ({
+        company,
+        contact,
+      }));
+    } catch (error) {
+      throw new BadRequestException(error);
     }
-
-    const contact = new SupplierContact();
-    //Object.assign(contact, createSupplierDto.supplierContact);
-    // await contact.save();
-
-    //const supplier = new Supplier();
-    // Object.assign(supplier, createSupplierDto);
-    // supplier.supplierContact = contact;
-    // supplier.company = company;
-
-    return this.create(createSupplierDto, Supplier, (dto) => ({
-      company,
-      contact,
-    }));
   }
 
   async updateSupplierContact(
     id: string,
     updateSupplierDto: UpdateSupplierDto,
   ): Promise<SupplierContact> {
-    const option: any = { id };
-    const entity = await this.supplierRepository.findOne({
-      where: option,
-    });
+    try {
+      const option: any = { id };
+      const entity = await this.supplierRepository.findOne({
+        where: option,
+      });
 
-    const entity2 = await this.supplierContactService.findById(
-      updateSupplierDto.supplierContact.id,
-    );
+      const entity2 = await this.supplierContactService.findById(
+        updateSupplierDto.supplierContact.id,
+      );
 
-    if (!entity || !entity2) throw new NotFoundException('Supplier not found!');
+      if (!entity || !entity2)
+        throw new NotFoundException('Supplier not found!');
 
-    entity2.firstName =
-      updateSupplierDto.supplierContact.firstName || entity2.firstName;
-    entity2.lastName =
-      updateSupplierDto.supplierContact.lastName || entity2.lastName;
-    entity2.email = updateSupplierDto.supplierContact.email || entity2.email;
-    entity2.dialcode =
-      updateSupplierDto.supplierContact.dialcode || entity2.dialcode;
-    entity2.phoneNumber =
-      updateSupplierDto.supplierContact.phoneNumber || entity2.phoneNumber;
+      entity2.firstName =
+        updateSupplierDto.supplierContact.firstName || entity2.firstName;
+      entity2.lastName =
+        updateSupplierDto.supplierContact.lastName || entity2.lastName;
+      entity2.email = updateSupplierDto.supplierContact.email || entity2.email;
+      entity2.dialcode =
+        updateSupplierDto.supplierContact.dialcode || entity2.dialcode;
+      entity2.phoneNumber =
+        updateSupplierDto.supplierContact.phoneNumber || entity2.phoneNumber;
 
-    return await entity2.save();
+      return await entity2.save();
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
   }
 }
